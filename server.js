46f5eadcccec1981a750a6afc2661c00d51de022
indrajeetgupta <indrajeetgupta231213@gmail.com>
2026-04-17 15:34:16 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000
 
 app.get('/', function (req, res)  {
   res.send('welcom to my hotels... How i can help you ?, we have list of menus')
@@ -20,6 +21,7 @@ app.use('/menuItem', MenuItemRoutes);
 
 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
