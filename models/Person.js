32b9cffaeab259a2ce2e0f1c5e9478e1e843f416
indrajeetgupta <indrajeetgupta231213@gmail.com>
2026-04-20 14:53:52 +0530

    updated project file and folders

--- models/Person.js
+++ models/Person.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
 
 const personSchema = new mongoose.Schema({
     name:{
@@ -30,9 +32,48 @@ const personSchema = new mongoose.Schema({
         type: String,
         required: true
 
+    },
+    username:{
+        requireld: true,
+        type: String
+
+    },
+    password:{
+        required: true,
+        type: String
     }
 
 });
+
+personSchema.pre('save', async  function(){
+    const person = this;
+    if(!person.isModified('password')) return ;
+    try{
+        //hash password generation
+
+        const salt = await bcrypt.genSalt(10); 
+
+        //hash password 
+        const hashedPassword = await bcrypt.hash(person.password, salt);
+
+        person.password  = hashedPassword;
+    } catch(err){
+        return next(err);
+
+    }
+})
+
+personSchema.methods.comparePassword = async  function (candidatePassword) {
+    try{
+
+        const isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    }catch(err){
+        throw err;
+
+    }
+
+}
 
 const person = mongoose.model('person', personSchema);
 module.exports = person;
--- server.js
+++ server.js
@@ -2,24 +2,33 @@ const express = require('express')
 const app = express()
 const db = require('./db');
 require('dotenv').config();
-
+const passport = require('./auth');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000
 
+//middleware function
+const logRequest = (req, res, next) => {
+  console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.orignalUrl}`);
+  next();
+}
+
+//auththentication
+app.use(logRequest);
+app.use(passport.initialize());
+const localAuthMiddleware = passport.authenticate('local', {session: false});
+
+
 app.get('/', function (req, res)  {
-  res.send('welcom to my hotels... How i can help you ?, we have list of menus')
+  res.send('welcome to our hotels');
 })
 
 
 const personRoutes = require('./routes/PersonRoutes');
 const MenuItemRoutes = require('./routes/MenuItemRoutes');
 
-app.use('/person', personRoutes);
-
+app.use('/person',localAuthMiddleware, personRoutes);
 app.use('/menuItem', MenuItemRoutes);
-
-
 
 
 app.listen(PORT, () => {
